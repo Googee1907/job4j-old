7ef526a5fed5160162708979061857beb5270d57
Googee1907 <googee@rambler.ru>
2019-08-20 18:25:57 +0300

    4.5. Отладка программы в IDEA [#162465]

--- chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqMaxTest.java
@@ -17,6 +17,48 @@ public class SqMaxTest {
         assertThat(result, is(expected));
     }
 
+    @Test
+    public void whenMaxSecond() {
+        int first = 15;
+        int second = 22;
+        int third = 16;
+        int forth = 16;
+        int expected = 22;
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
 
+    @Test
+    public void whenMaxThird() {
+        int first = 5;
+        int second = 2;
+        int third = 8;
+        int forth = 6;
+        int expected = 8;
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenMaxForth() {
+        int first = 35;
+        int second = 12;
+        int third = 28;
+        int forth = 88;
+        int expected = 88;
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenEqually() {
+        int first = 50;
+        int second = 50;
+        int third = 50;
+        int forth = 50;
+        int expected = 50;
+        int result = SqMax.max(first, second, third, forth);
+        assertThat(result, is(expected));
+    }
 }
 

1c10b7d37c032cdc238acc41b53d067ca2c8781f
Googee1907 <googee@rambler.ru>
2019-09-05 22:44:34 +0300

    5.7. JavaDoc. Code convention [#162472]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,20 +1,15 @@
 package ru.job4j.calculator;
-
-    public class Calculator {
+/**
+ * Class Calculator - решение задачи урока 1 части 001
+ * @author German Gordienko (googee@rambler.ru)
+ * @version 1.0
+ * @since 05.09.2019
+ */
+public class Calculator {
 
     public static void add(int first, int second) {
         int result =  first + second;
         System.out.println(first + " + " + second + " = " + result);
-    }
-
-    public static void div(int first, int second) {
-        int result =  first / second;
-        System.out.println(first + " / " + second + " = " + result);
-    }
-
-    public static void multiply(int first, int second) {
-        int result =  first * second;
-        System.out.println(first + " * " + second + " = " + result);
     }
 
     public static void subtrack(int first, int second) {
@@ -22,10 +17,26 @@ package ru.job4j.calculator;
         System.out.println(first + " - " + second + " = " + result);
     }
 
+    public static void multiply(int first, int second) {
+        int result =  first * second;
+        System.out.println(first + " * " + second + " = " + result);
+    }
+
+    public static void div(int first, int second) {
+        int result =  first / second;
+        System.out.println(first + " / " + second + " = " + result);
+    }
+    /**
+     * Main - арифметический метод
+     * функция add - сложение
+     * функция subtrack - вычитание
+     * функция multiply - умножение
+     * функция div - деление
+     */
     public static void main(String[] args) {
         add(1, 1);
-        div(4, 2);
-        multiply(2, 1);
         subtrack(10, 5);
+        multiply(2, 1);
+        div(4, 2);
     }
 }
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Package for calculate task
+ * @author German Gordienko (googee@rambler.ru)
+ * @version 1.0
+ * @since 05.09.2019
+ */
+package ru.job4j.calculator;

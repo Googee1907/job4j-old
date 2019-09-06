a4d29286bb64e885c1289e7f0fec939b1b7bb94a
Googee1907 <googee@rambler.ru>
2019-09-06 15:17:25 +0300

    3.0. Видео [#162500]

---
+++ chapter_001/src/main/java/ru/job4j/probe/course/Addition.java
@@ -0,0 +1,10 @@
+package ru.job4j.probe.course;
+
+public class Addition {
+    public static void main(String[] args) {
+        int first =  1;
+        int second = 2;
+        int result = first + second;
+        System.out.println(result);
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/probe/course/Comparison.java
@@ -0,0 +1,11 @@
+package ru.job4j.probe.course;
+
+public class Comparison {
+
+    public static void main(String[] args) {
+         int first =  1;
+         int second = 2;
+         boolean result = first > second;
+         System.out.println("Is first more than second? " + result);
+     }
+}

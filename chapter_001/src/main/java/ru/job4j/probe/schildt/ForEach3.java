0372527ae196b1473bb518ca6fc960fb09da25e0
Googee1907 <googee@rambler.ru>
2019-09-06 15:46:40 +0300

    6. Циклы [#162466]

---
+++ chapter_001/src/main/java/ru/job4j/probe/schildt/ForEach3.java
@@ -0,0 +1,20 @@
+package ru.job4j.probe.schildt;
+
+public class ForEach3 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int nums[][] = new int[3][5];
+        // присвоить значение элементам массива nums
+        for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 5; j++)
+                nums[i][j] = (i + 1) * (j + 1);
+        // использовать цикл for в стиле for each для вывода и суммирования значений
+        for(int x[] : nums) {
+            for(int y : x) {
+                System.out.println("Знaчeниe равно: " + y);
+                sum += y;
+            }
+        }
+        System.out.println("Cyммa: " + sum);
+    }
+}

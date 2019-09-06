ad8d2740036e6b6a52854775d2d1ab7b842679f7
Googee1907 <googee@rambler.ru>
2019-09-06 15:14:01 +0300

    3.1. Разбор первой программы. [#162452]

---
+++ chapter_001/src/main/java/ru/job4j/probe/course/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j.probe.course;
+
+public class StudentInfo {
+
+    public static void main (String[] args) {
+        System.out.println("German Gordienko");
+        System.out.println("19.07.1973");
+        }
+}

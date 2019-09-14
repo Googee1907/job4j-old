cc3de483d4d1102eedfaec77ea160ddb1bead62e
Googee1907 <googee@rambler.ru>
2019-09-14 19:28:11 +0300

    7. Массивы. [#162477]

--- chapter_001/src/main/java/ru/job4j/probe/schildt/arrays/TwoDArray.java
+++ chapter_001/src/main/java/ru/job4j/probe/schildt/arrays/TwoDArray.java
@@ -13,10 +13,6 @@ public class TwoDArray {
             for (j = 0; j < 5; j++) {
                 twoD[i][j] = k;
                 k++;
-            }
-        }
-        for (i = 0; i < 4; i++) {
-            for (j = 0; j < 5; j++) {
                 System.out.print(twoD[i][j] + " ");
             }
             System.out.println();

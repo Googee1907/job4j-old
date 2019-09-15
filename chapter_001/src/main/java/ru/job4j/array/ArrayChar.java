872abf6fe8cb2caf21a2de914cfad0ea5f475816
Googee1907 <googee@rambler.ru>
2019-09-16 00:58:18 +0300

    6.4. Слово начинается с ... [#162481]

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+/**
+ * Проверка соответствия слов
+ */
+public class ArrayChar {
+    public static boolean startsWith(char[] word, char[] pref) {
+        boolean result = true;
+
+        for (int i = 0; i < pref.length; i++) {
+            for (i = 0; i < pref.length; i++) {
+                if (word[i] != pref[i]) {
+                    result = false;
+                }
+            }
+        }
+        return result;
+    }
+}

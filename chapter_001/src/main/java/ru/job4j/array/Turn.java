3203aca4dfb09b7e02afaeb21e77de2e3c83b3cb
Googee1907 <googee@rambler.ru>
2019-09-20 01:53:33 +0300

    6.2. Перевернуть массив. [#162488]

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,14 +1,17 @@
 package ru.job4j.array;
 
 public class Turn {
+    /**
+     * Метод разворачивания массива наоборот
+     * @param array - исходный массив
+     * @return array - перевернутый массив
+     */
     public int[] back (int[] array) {
-        int temp;
-        // Проверим массив на четность/нечетность элементов
-        int half = array.length % 2 == 0 ? array.length / 2 : (array.length - 1) / 2;
-
+        int half = array.length / 2; // int отбрасывает дробную часть, поэтому это условие
+                                    // верно для четного и нечетного коичества элементов
         for (int i = 0; i <= half - 1; i++) {
-            // Запишем левый элемент во temp
-            temp = array[i];
+            // Запишем левый элемент в temp
+            int temp = array[i];
             // Присвоим в освободившийся индекс значение зеркального элемента
             array[i] = array[array.length - 1 - i];
             // Присвоим зеркальному элементу значение из temp
